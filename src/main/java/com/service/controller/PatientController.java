f9ee1d1b4bec86fde5a737c7d6540cf8c1cd1b0d
Touri Mohamed Amine <touri.med.amine@gmail.com>
2021-02-10 03:12:47 +0100

    afficherTous Editer

--- src/main/java/com/service/controller/PatientController.java
+++ src/main/java/com/service/controller/PatientController.java
@@ -1,9 +1,12 @@
 package com.service.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +40,18 @@ public class PatientController {
 		return patientRepository.chercherPatientParCIN(cin);
 	}
 	
-	
+	@GetMapping("/all")
+	public List<Patient> afficherTous(){
+		return patientRepository.findAll();
+		
+	}
+	@PutMapping("editer/{id}")
+	public Patient editerHospParId(@RequestBody Patient patient) {
+		Patient patientModifer = patientRepository.findById(patient.getId()).orElse(null);
+		patientModifer.setCin(patient.getCin());
+		patientModifer.setNom(patient.getNom());
+		patientModifer.setPrenom(patient.getPrenom());
+		return patientRepository.save(patientModifer);
+	}
 	
 }
